d61a768ee412d56ca417d2f2a0716f6111afea3c
guiyingbin <785901638@qq.com>
2022-10-07 10:29:22 +0800

    完成DeepFace Face alignment基本流程

--- Classification/FaceRecognition/DeepFace.py
+++ Classification/FaceRecognition/DeepFace.py
@@ -13,7 +13,16 @@ class deepface(nn.Module):
         self.model = self.build_model()
 
     def face_alignment(self, x):
-        # TODO 完成deep face中alignment部分
+        """
+        In th paper of DeepFace, the 2D alignment is just like face detection
+        3D alignment is to align faces undergoing out of-plane rotations, which is achieved by using
+        a second SVR to localize additional 67 fiducial points x2d in the 2D-aligned crop,
+        :param x:
+        :return:
+        """
+        # x2d = SVR(x)
+        # x3d = SVR(x)
+        # x = T(x)  T is a piece-wise affine transformation, and x is result of frontalization
         return x
 
     def build_model(self):
